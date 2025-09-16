c315a83fa16e1756070f217c711c1049d0fee015
Artem Kustov <dzenzinetta@yandex.ru>
2025-09-16 12:06:14 +0200

    chore: Formatted with Laravel/Pint

--- .section.php
+++ .section.php
@@ -1,9 +1,9 @@
 <?
 $sSectionName = 'Главная';
-$arDirProperties = array(
-	'title' => 'Title',
-	'description' => 'Description',
-	'keywords' => 'Keywords',
-	'robots' => 'index, follow'
-);
+ $arDirProperties = array(
+ 	'title' => 'Title',
+ 	'description' => 'Description',
+ 	'keywords' => 'Keywords',
+ 	'robots' => 'index, follow'
+ );
 ?>
